97e65de70d0b1317223b1be21dbca3f702dfd2c0
Edgar Alves <33017@alunos.isel.ipl.pt>
2023-06-04 19:06:13 +0100

    Removed unecessary files

--- app/src/main/java/com/codechallenge/nearshoretest/model/models/eventseriesstories/EventSeriesStoriesList.kt
+++ app/src/main/java/com/codechallenge/nearshoretest/model/models/eventseriesstories/EventSeriesStoriesList.kt
@@ -1,4 +1,4 @@
-package com.codechallenge.nearshoretest.model.models.events
+package com.codechallenge.nearshoretest.model.models.eventseriesstories
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
@@ -9,5 +9,5 @@ data class EventsList(
     @SerializedName("available")
     val available : Int,
     @SerializedName("items")
-    val items : List<Event>
+    val items : List<EventSeriesStories>
 ): Parcelable
