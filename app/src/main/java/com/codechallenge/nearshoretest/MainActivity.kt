5dfa519bc69bae85fbf5833c8f3d30deddc168ac
Edgar Alves <33017@alunos.isel.ipl.pt>
2023-06-04 11:27:44 +0100

    BottomNavigationDrawer

--- app/src/main/java/com/codechallenge/nearshoretest/MainActivity.kt
+++ app/src/main/java/com/codechallenge/nearshoretest/MainActivity.kt
@@ -21,19 +21,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        navView = binding.bottomNavigationView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_about
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        hideActionBar()
     }
 
     fun changeBottomNavigationVisibility(show: Boolean) {
