41b114ab372dc2daa87c77277bcf02ea5982dc10
Edgar Alves <33017@alunos.isel.ipl.pt>
2023-06-04 19:40:35 +0100

    Deleted uncessary comments

--- app/src/main/java/com/codechallenge/nearshoretest/model/models/comics/Comic.kt
+++ app/src/main/java/com/codechallenge/nearshoretest/model/models/comics/Comic.kt
@@ -1,25 +1,16 @@
 package com.codechallenge.nearshoretest.model.models.comics
 
 import android.os.Parcelable
+import com.codechallenge.nearshoretest.model.models.thumbnails.Thumbnail
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
-/*
-"available": 12,
-          "collectionURI": "http://gateway.marvel.com/v1/public/characters/1011334/comics",
-          "items": [
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/comics/21366",
-              "name": "Avengers: The Initiative (2007) #14"
-            },
-            ...
-          ],
- */
-
 @Parcelize
 data class Comic(
-    @SerializedName("resourceURI")
-    val resourceURI : String,
-    @SerializedName("name")
-    val name : String
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title : String,
+    @SerializedName("thumbnail")
+    val thumbnail: Thumbnail
 ): Parcelable
